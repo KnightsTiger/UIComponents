9a0ee3da6ad1bdee0930395082a15a2932e68132
knight <tocaldera6734@gmail.com>
2018-12-07 13:11:04 +0530

    UI components - button,textInput Layout,image button

--- app/src/main/java/com/example/knight/uicomponents/MainActivity.java
+++ app/src/main/java/com/example/knight/uicomponents/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.knight.uicomponents;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToPageTwo(View view) {
+        startActivity(new Intent(this,PageTwo.class));
+    }
+
+    public void playIt(View view) {
+
+/*        MediaPlayer mediaPlayer = MediaPlayer.create(this,R);
+        mediaPlayer.start(); */// no need to call prepare(); create() does that for you
     }
 }
