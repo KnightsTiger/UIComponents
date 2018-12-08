2213fc41763d4656b89e2d88b81f3bcce5f36f84
knight <tocaldera6734@gmail.com>
2018-12-08 07:32:43 +0530

    Alert Dialog, Custom dialog

--- app/src/main/java/com/example/knight/uicomponents/ActivityThree.java
+++ app/src/main/java/com/example/knight/uicomponents/ActivityThree.java
@@ -1,7 +1,11 @@
 package com.example.knight.uicomponents;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.Objects;
@@ -14,5 +18,32 @@ public class ActivityThree extends AppCompatActivity {
         setContentView(R.layout.activity_three);
         TextView textView=  findViewById(R.id.textView4);
         textView.setText(Objects.requireNonNull(getIntent().getExtras()).getString("key"));
+    }
+
+    public void doNotAgree(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Wanna exit?");
+        builder.setCancelable(false);
+        builder.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+               ActivityThree.this.finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    public void showCustom(View view) {
+        Dialog dialog = new Dialog(this);
+        //set the layout for reqied layout
+        dialog.setContentView(R.layout.customalert);
+        dialog.show();
     }
 }
--- app/src/main/java/com/example/knight/uicomponents/MainActivity.java
+++ app/src/main/java/com/example/knight/uicomponents/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.knight.uicomponents;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
     public void goToPageTwo(View view) {
